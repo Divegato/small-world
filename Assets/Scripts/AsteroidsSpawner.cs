0b4624fb76b048c0780ab95f6e717fc002cd3152
Brandon Salmon <brandon.j.salmon@gmail.com>
2017-04-22 15:08:03 -0500

    Asteroids

---
+++ Assets/Scripts/AsteroidsSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AsteroidsSpawner : MonoBehaviour
+{
+    public GameObject Asteroid;
+    public float SpawnRate = 1;
+
+    private float progress = 0;
+
+    void Start()
+    {
+        progress = 0;
+    }
+
+    void Update()
+    {
+        progress += Time.deltaTime;
+
+        if (progress >= SpawnRate)
+        {
+            progress = 0;
+            SpawnAsteroid();
+        }
+    }
+
+    private void SpawnAsteroid()
+    {
+        var selfSize = GetComponent<Renderer>().bounds;
+
+        var x = (Random.value * selfSize.size.x) - selfSize.extents.x;
+        var y = (Random.value * selfSize.size.y) - selfSize.extents.y;
+
+        var position = new Vector3(x, y, 0);
+
+
+        var result = Instantiate(Asteroid, position, Quaternion.identity);
+        var body = result.GetComponent<Rigidbody2D>();
+
+        var randomVelocity = (Random.value * 10f) + 5f;
+        body.AddForce(Random.insideUnitCircle * randomVelocity, ForceMode2D.Impulse);
+    }
+}
