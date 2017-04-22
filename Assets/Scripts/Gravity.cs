7742c6c0c30c01364434f810929ca02844288e37
Brandon Salmon <brandon.j.salmon@gmail.com>
2017-04-21 22:55:19 -0500

    Experimental prototype

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float GravityPower = 1;
+    public float GravityRange = 10;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var self = this.gameObject.transform.position;
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+        var vector2 = new Vector2(self.x, self.y);
+
+        var difference = vector2 - player.position;
+        var percent = Mathf.Min(1, Mathf.Exp(difference.magnitude * (-1f / GravityRange)));
+
+        player.AddForce(difference.normalized * GravityPower * percent, ForceMode2D.Force);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public KeyCode UpKey = KeyCode.UpArrow;
+    public KeyCode DownKey = KeyCode.DownArrow;
+    public KeyCode LeftKey = KeyCode.LeftArrow;
+    public KeyCode RightKey = KeyCode.RightArrow;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var body = GetComponent<Rigidbody2D>();
+        var direction = Vector2.zero;
+
+        if (Input.GetKeyDown(this.UpKey))
+        {
+            direction = body.transform.up;
+        }
+        if (Input.GetKeyDown(this.DownKey))
+        {
+            direction = body.transform.up * -1;
+        }
+        if (Input.GetKeyDown(this.LeftKey))
+        {
+            direction = Vector2.left;
+        }
+        if (Input.GetKeyDown(this.RightKey))
+        {
+            direction = Vector2.right;
+        }
+
+        body.AddForce(direction, ForceMode2D.Impulse);
+    }
+}
