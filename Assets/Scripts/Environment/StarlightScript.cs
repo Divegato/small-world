ef86acff272888dacf816ea662cbad0610941d42
Brandon <brandon.j.salmon@gmail.com>
2021-01-04 16:57:04 -0600

    Add directional lighting

---
+++ Assets/Scripts/Environment/StarlightScript.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Helpers;
+using UnityEngine;
+
+public class StarlightScript : MonoBehaviour
+{
+    public GameObject Starlight;
+    public float Radius = 10000;
+    public float Quantity = 100;
+
+    void Start()
+    {
+        for (int i = 0; i < Quantity; i++)
+        {
+            var position = Geometry.GetRandomPointOnCircle(Radius);
+            Instantiate(Starlight, position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
